9f04e09be23e4e952f1dd00e426a495118a5634b
dogg0-hub <8equal18@gmail.com>
2025-05-16 12:11:08 +0900

    CRUD

--- nextStudy/my-app/src/app/users/route.ts
+++ nextStudy/my-app/src/app/users/route.ts
@@ -1,7 +1,9 @@
+import { json } from "stream/consumers";
 import { User } from "../../types";
 import data from "./data.json"
 import fs from "fs"
 import path from "path";
+import { NextRequest } from "next/server";
 
 export async function GET() {
     return new Response(JSON.stringify(data))
@@ -10,7 +12,7 @@ export async function GET() {
 export async function POST (request :Request) {
     try{
         const user = await request.json();
-        const filePath = path.join(process.cwd(),"src/app/users/data.json")
+        const filePath = path.join(process.cwd(),"src/app/users/data.json");
         const newUser = {
             id:data.length + 1,
             name:user.name,
@@ -24,7 +26,35 @@ export async function POST (request :Request) {
             status : 201,
         })
     }catch(error){
-
-    }//try-catch でエラーハンドリングしていく
+        if(error instanceof SyntaxError){
+            return new Response("書き方がおかしいです",{ status: 400});
+        }if(error instanceof TypeError){
+            return new Response("その型は入れられないです",{ status: 500});
+        }
+        return new Response("予期しないエラーが発生しました", { status : 500});
+    }
     
+}
+
+export async function PATCH(request:Request) {
+    const user = await request.json();
+    const filePath = path.join(process.cwd(),"src/app/users/data.json");
+    var arrayData = fs.readFileSync(filePath, "utf-8");
+    var arrayContents = JSON.parse(arrayData);
+
+    const index = arrayContents.findIndex((u:User) => u.id === user.id);
+    if(index === -1){
+        return new Response("ユーザーがいません",{ status:404 });
+    }
+    arrayContents[index] = {
+        id:user.id,
+        name:user.name,
+        password:user.password,
+        description:user.description,
+    };
+    fs.writeFileSync(filePath, JSON.stringify(arrayContents,null,2 ));
+    return new Response(JSON.stringify(arrayContents[index]),{
+        headers: { "Content-Type" : "application/json"},
+        status : 200,
+    })
 }
