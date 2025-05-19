cd7a6ee1f1dfd13e277464b34add7eb13ca5949e
dogg0-hub <8equal18@gmail.com>
2025-05-19 13:06:48 +0900

    change

--- nextStudy/my-app/src/app/api/sales/route.js
+++ nextStudy/my-app/src/app/api/sales/route.js
@@ -14,29 +14,15 @@ export async function GET() {
     return NextResponse.json(sales);
 }
 
-// export async function POST(request) {
-//     const body = await request.json();
-//     const client = await clientPromise;
-//     const db = client.db("users_db");
-//     const result = await db
-//                 .collection("users")
-//                 .insertOne(body);
-//     return new Response(JSON.stringify(result),{
-//             headers: { "Content-Type" : "application/json"},
-//             status : 201,
-//         })
-// }
-
-
-
 export async function POST(request) {
-  try {
     const body = await request.json();
     const client = await clientPromise;
     const db = client.db("users_db");
-    const result = await db.collection("users").insertOne(body);
-    return NextResponse.json({ message: "User added", id: result.insertedId }, { status: 201 });
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+    const result = await db
+                .collection("users")
+                .insertOne(body);
+    return new Response(JSON.stringify(result),{
+            headers: { "Content-Type" : "application/json"},
+            status : 201,
+        })
 }
