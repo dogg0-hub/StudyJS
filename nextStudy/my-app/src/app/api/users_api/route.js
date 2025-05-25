51a1ab8ef1d3c9c67edf9682ef58e315958f7353
dogg0-hub <8equal18@gmail.com>
2025-05-25 23:51:59 +0900

    fixed the error

--- nextStudy/my-app/src/app/api/users_api/route.js
+++ nextStudy/my-app/src/app/api/users_api/route.js
@@ -1,9 +1,6 @@
-"use client"
 import clientPromise from "@/lib/mongodb";
 import { MongoClient } from "mongodb";
 import { NextResponse,NextRequest } from "next/server";
-
-
 
 export async function GET() {
     const client = await clientPromise;
@@ -46,6 +43,29 @@ export async function DELETE(request) {
     const result = await db
                   .collection("users")
                   .deleteOne({id : idUser});
+    return new Response(JSON.stringify(result),{
+            headers: { "Content-Type" : "application/json"},
+            status : 200,
+        })
+}
+
+export async function PUT(request) {
+    const body = await request.json();
+    const client = await clientPromise;
+    const db = client.db("users_db");
+    const idUser = Number(body.id);
+
+    if(idUser == body.id){
+        const user= {
+        "id": body.id,
+        "name":body.name,
+        "password":body.password,
+        "description":body.description,
+    }
+        const result = await db
+                  .collection("users")
+                  .updateOne({id: idUser},user);
+    }
     return new Response(JSON.stringify(result),{
             headers: { "Content-Type" : "application/json"},
             status : 200,
